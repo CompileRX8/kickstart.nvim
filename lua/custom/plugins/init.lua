453fcf92a5f793cac92581f35b7baf2de9a63431
Ryan Highley <rhighley@redhat.com>
2026-06-10 11:57:14 -0500

    Updated Java LSP, Added modus themes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,31 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  {
+    'nvim-java/nvim-java',
+    config = function()
+      require('java').setup()
+      vim.lsp.enable 'jdtls'
+    end,
+  },
+  {
+    'miikanissi/modus-themes.nvim',
+    priority = 1000,
+    config = function()
+      require('modus-themes').setup {
+        style = 'auto',
+        variants = {
+          modus_operandi = 'default',
+          modus_vivendi = 'tinted',
+        },
+        transparent = true,
+        dim_inactive = true,
+        hide_inactive_statusline = false,
+        line_nr_column_background = false,
+        sign_column_background = false,
+      }
+      vim.cmd [[colorscheme modus]]
+    end,
+  },
+}
